e06c349bc9adf9296a0757d4c1f3c92d78a6af93
jbuberel <jason@buberel.org>
2015-06-20 13:40:23 -0700

    Benchmark using random words

--- test_test.go
+++ test_test.go
@@ -1,11 +1,39 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"os"
+  "log"
+  "math/rand"
 
-var str = "Thompson"
-var sub = "son"
+	"testing"
+)
+
+
 func BenchmarkStr(b *testing.B) {
-    for n := 0; n < b.N; n++ {
-        substring(str, sub)
-    }
+  words, _ := readLines("wordlist.txt")
+  rand.Seed(37)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+    left := words[rand.Intn(len(words))]
+    right := words[rand.Intn(len(words))]
+    log.Printf("[%v] [%v]\n", left, right)
+    substring(left, right)
+
+	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
